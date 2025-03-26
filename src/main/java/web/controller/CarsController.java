38eb6e9ec15e62c489d46f9fe92e076a04e812ce
Firsdv <firsdv8@gmail.com>
2025-03-26 20:25:31 +0300

    Исправил и сделал вывод /cars

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -3,7 +3,7 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
@@ -18,8 +18,8 @@ public class CarsController {
         this.carService = carService;
     }
 
-    @GetMapping(value = "/cars")
-    public String printCars(@RequestParam("count") int count, ModelMap model) {
+    @GetMapping("/cars")
+    public String printCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
         model.addAttribute("cars", carService.getCar(count));
         return "cars";
     }
